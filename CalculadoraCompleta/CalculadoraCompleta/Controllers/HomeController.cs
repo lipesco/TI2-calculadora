9a0bbc37200792c88051ae860a2c45dbe7f39f40
lipesco <filipe.branco@gmail.com>
2017-03-28 13:09:07 +0100

    adicionado os operadores

--- CalculadoraCompleta/CalculadoraCompleta/Controllers/HomeController.cs
+++ CalculadoraCompleta/CalculadoraCompleta/Controllers/HomeController.cs
@@ -11,9 +11,15 @@ namespace CalculadoraCompleta.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            ViewBag.visor = 0;
+            //inicializar valores
+            ViewBag.Visor = 0;
+            //ViewBag.Visor = "0";
+            Session["PrimeiroOperador"] = true;
             return View();
         }
+
+        //string op1, op2, operador;
+        //int resultado;
 
         // POST: Home
         [HttpPost]
@@ -34,12 +40,55 @@ namespace CalculadoraCompleta.Controllers
                     //determinar se o visor só existe um zero
                     if (visor.Equals("0")) visor = bt;
                     else visor += bt; //visor=visor+bt;
-                
-                
+                    break;
+                case ",":
+                    if (!visor.Contains(",")) visor += ",";
+                    break;
+                case "+/-":
+                    //visor = Convert.ToDouble(visor)*-1+"";   //visor é uma string, converte para double, e adiciona string para tornar de novo uma string
+                    if (visor.StartsWith("-")) visor = visor.Replace("-", "");
+                    else
+                        if(!visor.Equals("0"))
+                            visor = "-" + visor;
+                    break;
+                case "C":
+                    visor = "0";
+                    Session["PrimeiroOperador"] = true;
+                    break;
+                case "+":
+                case "-":
+                case "x":
+                case ":":
+                    if((bool)Session["PrimeiroOperador"]) { 
+                        //guardar o valor do visor
+                        //op1 = visor;
+                        Session["operando"] = visor;
+                        //limpar o visor
+                        visor = "0";
+                        //guardar o operador
+                        //operador = bt;
+                        Session["operador"] = bt;
+                        //marcar como tendo utilizado o operador
+                        Session["PrimeiroOperador"] = false;
+                    }
+                    else
+                    {
+                        //se não é a primeira vez que se clica num operador, 
+                        //vou utilizar os valores anteriores
+                        switch ((string)Session["operador"])
+                        {
+                            //recuperador código da primeira calculador
+                        }
+                        //guardar os novos valores...
+
+                    }
+                    break;
+
+                default:
                     break;
             }
             //entregar o novo valor à vew
-            ViewBag.visor = visor;
+            ViewBag.Visor = visor;
             return View();
         }
     }
